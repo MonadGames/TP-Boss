01b6e84154611fca89bc4b40e14bd5c3df8d639c
Fede Lochbaum <federico.lochbaum@gmail.com>
2017-08-23 12:54:47 -0300

    proyecto base, paredes, piedra (falta densidad), main camara sigue al personaje, no gira con el.

---
+++ Game/Assets/Scripts/Character.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public float speed;
+
+	private bool isDead = false;
+
+	private bool isRight;
+
+	private Rigidbody2D rb;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		isRight = true;
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isDead) { 
+			float horizontal = Input.GetAxis ("Horizontal");
+
+			rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
+
+			if (rb.velocity.x != 0) {
+				anim.SetTrigger ("Run");
+			} else if (rb.velocity.x > 0 && rb.velocity.y > 0) {
+				anim.SetTrigger ("JumpWithVelocity");
+			} else {
+				anim.SetTrigger ("Pause");
+			}
+
+			Flip (horizontal);
+		}
+	}
+
+	void OnCollisionEnter2D (){
+		isDead = false; 
+	}
+
+	private void Flip(float horizontal){
+		if (horizontal > 0 && !isRight || horizontal < 0 && isRight) {
+			isRight = !isRight;
+
+			Vector3 scale = transform.localScale;
+			scale.x *= -1;
+			transform.localScale = scale;
+		}
+	}
+}
---
+++ Game/Assets/Scripts/Jump.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	[Range(1, 10)]
+	public float jumpVelocity;
+
+	[Range(-5, 2)]
+	public float maxJump;
+
+	private Animator anim;
+
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump") && (rb.position.y < maxJump)) {
+			rb.velocity = Vector2.up * jumpVelocity;
+			anim.SetTrigger ("Jump");
+		} else if(rb.position.y < 0){
+			anim.SetTrigger ("Pause");
+		}
+	}
+}
