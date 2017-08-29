b9fbe79e58d57f170c121ac50964be0197af9cf0
Gabriel <gabriel.l.maljkovich@gmail.com>
2017-08-29 10:41:07 -0300

    Movimiento decente (codigo feo)

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -9,26 +9,35 @@ public class Character : MonoBehaviour {
 	private bool isRight;
 	private Rigidbody2D rb;
 	private Animator anim;
+	private bool walking;
 
 	void Start () {
 		isRight = true;
+		walking = false;
 		rb = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 	}
 
 	void FixedUpdate () {
-		rb.rotation = 0;
 		if (!isDead) {
 			checkForMove ();
-			checkForActions ();
 			flip ();
 		}
 	}
 
 	private void checkForMove(){
+		// move();
 		// faltaria hacer que la velocidad cambie usando la aceleracion. Asi pasa de caminar a correr
-		var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
-		transform.position += move * speed * Time.deltaTime;
+		if (shouldMove()) {
+			anim.SetTrigger ("Walk");
+			move ();
+			walking = true;
+		} else {
+			anim.SetTrigger ("Idle");
+			rb.velocity = new Vector2(0, rb.velocity.y);
+			walking = false;
+		}
+
 	}
 
 	void OnCollisionEnter2D (){
@@ -36,11 +45,20 @@ public class Character : MonoBehaviour {
 	}
 
 	private void checkForActions(){
-		if (rb.velocity.x != 0) {
+		if (rb.velocity.x > 0) {
 			anim.SetTrigger ("Walk");
-		} else {
+		} else if(rb.velocity.x == 0){
 			anim.SetTrigger ("Idle");
 		}
+	}
+
+	private void move(){
+		var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+
+	private bool shouldMove(){
+		return Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow);
 	}
 
 	private void flip(){
