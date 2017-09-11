c27aba2c8ac278815879b2a29b1ab9c7c145f182
Gabriel Maljkovich <gm@camba.coop>
2017-09-10 21:47:11 -0300

    Speech bubble

---
+++ Assets/Scripts/Character/SpeechTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpeechTrigger : MonoBehaviour
+{
+	[SerializeField]
+	Canvas messageCanvas;
+	bool isShowingMessage = false;
+
+	void Start()
+	{
+		messageCanvas.enabled = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+//		if (other.name == "Player")
+//		{
+//			TurnOnMessage();
+//		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.name == "Player" && Input.GetKeyDown (KeyCode.E)) {
+			TurnOnMessage();                                
+		}
+	}
+
+	private void TurnOnMessage()
+	{
+		if (!isShowingMessage) {
+			messageCanvas.enabled = true;
+			isShowingMessage = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player")
+		{
+			TurnOffMessage();
+		}
+	}
+
+	private void TurnOffMessage()
+	{
+		messageCanvas.enabled = false;
+		isShowingMessage = false;
+	}
+}
