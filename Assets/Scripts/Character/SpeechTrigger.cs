fcbc5117a098327dcb6916f72daafb2b2b00010a
Gabriel Maljkovich <gm@camba.coop>
2017-09-13 12:24:52 -0300

    Type-writing effect

--- Assets/Scripts/Character/SpeechTrigger.cs
+++ Assets/Scripts/Character/SpeechTrigger.cs
@@ -8,34 +8,34 @@ public class SpeechTrigger : MonoBehaviour
 {
 	public GameObject panel;
 	public GameObject hint;
+	public Text text;
+	private TW_MultiStrings_Regular typewriter;
+	private int stringCount;
+	private int currentString;
 	bool isShowingMessage = false;
 
 	void Start()
 	{
+		typewriter = text.GetComponent<TW_MultiStrings_Regular> ();
+		stringCount = typewriter.MultiStrings.Length;
+		currentString = 1;
 		panel.SetActive(false);
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.name == "Player")
-		{
-			hint.SetActive (true);
-		}
+		showHint (other.name == "Player");
 	}
 
 	void OnTriggerStay2D(Collider2D other)
 	{
-		if (other.name == "Player" && Input.GetKeyDown (KeyCode.E)) {
+		if (shouldShowMessage(other)) {
 			TurnOnMessage();  
-			hint.SetActive (false);
+		} else if (shouldShowNextString(other)) {
+			showNextString ();
 		}
-	}
-
-	private void TurnOnMessage()
-	{
-		if (!isShowingMessage) {
-			panel.SetActive(true);
-			isShowingMessage = true;
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			hideMessage (true);
 		}
 	}
 
@@ -47,10 +47,43 @@ public class SpeechTrigger : MonoBehaviour
 		}
 	}
 
+	private void showNextString() {
+		typewriter.NextString ();
+		currentString++;
+	}
+
+	private void showHint(bool show){
+		hint.SetActive (show);
+	}
+
+	private void hideMessage(bool showTheHint){
+		if (isShowingMessage){
+			panel.SetActive (false);
+			isShowingMessage = false;
+			showHint (showTheHint);
+		}
+	}
+
+	private bool shouldShowMessage(Collider2D other){
+		return other.name == "Player" && Input.GetKeyDown (KeyCode.E) && !isShowingMessage;
+	}
+
+	private bool shouldShowNextString (Collider2D other){
+		return other.name == "Player" && Input.GetKeyDown (KeyCode.E) && currentString < stringCount;
+	}
+
+	private void TurnOnMessage()
+	{
+		panel.SetActive(true);
+		showHint(false);
+		isShowingMessage = true;
+		typewriter.StartTypewriter ();
+	}
+
 	private void TurnOffMessage()
 	{
-		panel.SetActive(false);
-		hint.SetActive (false);
-		isShowingMessage = false;
+		hideMessage (false);
+		currentString = 1;
+		typewriter.pointer = 0;
 	}
 }
