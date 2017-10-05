1dd97b05a6b93520f3154a7bbd4c69c83f5e395a
Fede Lochbaum <federico.lochbaum@gmail.com>
2017-10-05 11:23:13 -0300

    transparencia del enemy cuando muere

--- Assets/Scripts/Character/Enemy/Enemy.cs
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class Enemy : Character {
 
 	private EnemyAI enemyAI;
+	public SpriteRenderer spriteRenderer;
+	public float deadSpeed = 0.0000000000001f;
+	public float timeOfDead = 1f;
+	private bool isAlive;
 	
 	void Start () {
 		health = gameObject.GetComponent<Health>();
 		enemyAI = gameObject.GetComponent<EnemyAI>();
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		isAlive = true;
 	}
 
 	void Update () {
-		if(health.health <= 0)
-			Destroy(gameObject, 1f);
+		checkDead ();
+	}
+		
+	public void checkDead(){
+		if(isDead()) {
+			Color color = spriteRenderer.material.color;
+			print (color.a);
+			if (timeOfDead > 0) {
+				color.a = timeOfDead;
+				timeOfDead -= Time.deltaTime;
+
+				spriteRenderer.material.color = color;
+			} else {
+				Destroy(gameObject, 0f);
+			}
+		}
+	}
+
+	public void die(){
+		// No esta llegando aca
+		isAlive = false;
+		//Aca definir la accion de la muerte del enemy
 	}
 
 	public void move(){
@@ -32,10 +58,6 @@ public class Enemy : Character {
 		if (collision.gameObject.tag == "spell") {
 			enemyAI.takeDamage ();
 		}
-	}
-
-	public void die(){
-		//Aca definir la accion de la muerte del enemy
 	}
 
 }
