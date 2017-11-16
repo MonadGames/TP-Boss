bccad0812fef4d25876fad7419142a5a7758cbce
Fede Lochbaum <federico.lochbaum@gmail.com>
2017-11-16 14:14:37 -0300

    Ahora el drop de los mobs sigue al player cuando esta a cierta distancia

--- Assets/Scripts/UI/BubbleLoot.cs
+++ Assets/Scripts/UI/BubbleLoot.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public abstract class BubbleLoot : MonoBehaviour {
 
-	protected float value;
+	[Range(0, 3)]
+	public float distance;
 
+	[Range(1, 2)]
+	public float speed;
+
+	protected float value;
 	private bool give = false;
 
 	protected FloatingTextController textController;
@@ -13,11 +18,26 @@ public abstract class BubbleLoot : MonoBehaviour {
 
 	public AudioClip sound;
 	private AudioSource source;
+	private Vector3 target;
 
 	void Start() {
 		textController = GameObject.FindObjectOfType<FloatingTextController> ();
 		player = GameObject.FindObjectOfType<Player> ();
 		source = GetComponent<AudioSource> ();
+		target = player.transform.position;
+		target.y += 0.5f;
+	}
+
+	void Update() {
+		checkFollow ();
+	}
+
+	public void checkFollow() {
+		float range = Vector2.Distance (transform.position, player.transform.position);
+		 
+		if(range <= distance) {
+			transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+		}
 	}
 
 
