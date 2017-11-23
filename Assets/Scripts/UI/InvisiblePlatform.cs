ab4bccc543f1be655bade252775fb41797c88770
Fede Lochbaum <federico.lochbaum@gmail.com>
2017-11-22 23:21:44 -0300

    agregado en prefab Boss y creacion de prefab InvisibleFloor

---
+++ Assets/Scripts/UI/InvisiblePlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisiblePlatform : MonoBehaviour {
+
+	public GameObject platform;
+	public float visibleDistance;
+
+	private Player player;
+
+	void Start () {
+		player = GameObject.FindObjectOfType<Player> ();
+	}
+
+	void Update () {
+		checkVisibility ();
+	}
+
+	public void checkVisibility() {
+		float distance = Vector2.Distance (transform.position, player.transform.position);
+
+		if (distance <= visibleDistance) {
+			platform.SetActive (true);
+		}
+	}
+}
