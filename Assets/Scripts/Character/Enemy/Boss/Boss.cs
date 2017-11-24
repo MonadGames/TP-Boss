cd316324ee17fc19e703865ae5ac875a50658d5d
Fede Lochbaum <federico.lochbaum@gmail.com>
2017-11-24 09:39:40 -0300

    Animaciones del boss, configuracion del mismo y posicionamiento en un piso flotante.

--- Assets/Scripts/Character/Enemy/Boss/Boss.cs
+++ Assets/Scripts/Character/Enemy/Boss/Boss.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss : MonoBehaviour {
-
+public class Boss : Character {
+	
+	public float cd = 0.5f;
+	public float lastTime = 0.5f;
 	public float rangeOfAwake;
-	private BossState state;
+	public AudioClip hitSound;
 
+	private BossState state;
+	private AudioSource source;
 	private ChargeEnemy iaAttack;
 	private Player player;
 	private Animator anim;
@@ -17,10 +21,12 @@ public class Boss : MonoBehaviour {
 		iaAttack = GetComponent<ChargeEnemy> ();
 		player = GameObject.FindObjectOfType<Player> ();
 		state = new AsleepState (this);
+		source = GetComponent<AudioSource> ();
 	}
 
 	void Update () {
 		state.update ();
+		lastTime += Time.deltaTime;
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
@@ -43,5 +49,19 @@ public class Boss : MonoBehaviour {
 
 	public void setState(BossState newState) {
 		state = newState;
+	}
+
+	public void attack(Player player){
+		player.takeDamage(Random.Range(damage * 0.8f, damage), transform);
+	}
+
+	public void OnCollisionStay2D (Collision2D collision) {
+		if (iaAttack.enabled) {
+			if (collision.gameObject.name == "Player" && lastTime >= cd) {
+				attack (collision.gameObject.GetComponent<Player> ());
+				source.PlayOneShot (hitSound, 1f);
+				lastTime = 0;
+			}
+		}
 	}
 }
