bb090f5c3d18db42285dcf40eeaf5517f0103089
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2017-11-28 21:37:24 -0300

    Cargar juego despues de intro

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+	public float seconds = 8f;
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine (changeScene ());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public IEnumerator changeScene(){
+		yield return new WaitForSeconds (seconds);
+		SceneManager.LoadScene("Scene3");
+	}
+}
+
