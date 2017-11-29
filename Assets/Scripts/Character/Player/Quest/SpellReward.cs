b3eaac5b3393aa16a68c1cf86d3236e46a47a6d4
Fede Lochbaum <federico.lochbaum@gmail.com>
2017-11-29 01:08:53 -0300

    los spells te dan una accio accion buena o mala

--- Assets/Scripts/Character/Player/Quest/SpellReward.cs
+++ Assets/Scripts/Character/Player/Quest/SpellReward.cs
@@ -6,8 +6,16 @@ public class SpellReward : Reward
 {
 	public GameObject spell;
 
+	public bool goodAction;
+	public bool badAction;
+
 	public override void applyReward(Player player) {
 		player.addSpell (spell);
+		if (goodAction) {
+			player.addGoodAction ();
+		} else if(badAction) {
+			player.addBadAction ();
+		}
 	}
 }
 
